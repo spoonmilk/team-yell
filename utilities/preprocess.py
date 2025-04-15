b4977fc6aff84d670365a1dde57fb1c6a0186dd3
William Stone <william_l_stone@brown.edu>
2025-04-15 16:28:15 -0400

    Changed preprocess so that it now returns audio data as Log Mel Spectrograms (like whisper) and fixed a couple small bugs

--- utilities/preprocess.py
+++ utilities/preprocess.py
@@ -1,15 +1,20 @@
 import torch as pt
 #from multiprocessing import Pool, Lock, Process, Manager
+from pathlib import Path
 import threading
 from concurrent.futures import ThreadPoolExecutor
 import os
 from typing import Iterator, Any
+from collections.abc import Callable
 import re
 import random
 import torchaudio
 #from dataclasses import dataclass
 
-AUDIO_DIR = os.path.abspath("../LibriSpeech/dev-clean") + "/" #Path to the dev-clean directory of your downloaded LibriSpeech dataset
+try: #Path to LibriSpeech dev-clean folder
+    AUDIO_DIR = str(Path(__file__).resolve().parent.parent) + "/LibriSpeech/dev-clean/" #Will work in normal use cases (running this file directly, importing)
+except NameError:
+    AUDIO_DIR = os.path.abspath("../LibriSpeech/dev-clean") + "/" #Works for when this file gets run interactively (import by interactive python interpreter)
 NUM_WORKERS = 5 #Number of threads you're willing to spawn to parallelize audio file reading
 
 # DATATYPES
@@ -46,14 +51,15 @@ def find_transcription(flac_file: str) -> str:
             if re.match(file_name, line): return re.sub(r"^[^\s]*\s", "", line).rstrip()
     raise Exception(f"No transcription for {file_name} found in {trans_file}")
         
-def extract_data(audio_paths: Locked, read_files: Locked, file_transcriptions: Locked):
-    """Pops a random path to an audio file from the input thread-safe list of them, reads its audio data, finds its
+def extract_data(audio_paths: Locked, read_files: Locked, file_transcriptions: Locked, audio_trans: Callable[[pt.Tensor],pt.Tensor] = None):
+    """Pops a random path to an audio file from the input thread-safe list of them, reads (and transforms) its audio data, finds its
     transcription, and then appends these two pieces of data to their respective input thread-safe lists"""
     with audio_paths.lock:
         path = audio_paths.inner.pop(random.randrange(len(audio_paths.inner)))
     file = AUDIO_DIR + path
     audio_data, _ = torchaudio.load(file) #Loads in time series data of the file as a pytorch tensor
-    audio_data = audio_data[0] #Returned tensors from load are shape (1, length), so just down-dimming them to shape (length) here
+    if audio_trans: audio_data = audio_trans(audio_data)
+    audio_data = audio_data[0] #Returned tensors from load are shape (1, max_amplitude (80), length (in time)), so just down-dimming them to shape (max_amplitude, length) here
     trans = find_transcription(file).split(" ")
     with read_files.lock, file_transcriptions.lock: #Both locks must be obtained at the same time to ensure proper ordering
         read_files.inner.append(audio_data)
@@ -63,24 +69,30 @@ def extract_data(audio_paths: Locked, read_files: Locked, file_transcriptions: L
 
 def grab_audio(num_files: int) -> tuple[pt.Tensor, list[list[str]]]:
     """Grabs num_files many random audio files from LibriSpeech, reads (and pads) all of their data into a tensor of shape
-     (num_files, max_audio_data_length), creates an associated list of transcriptions, and returns the two"""
+     (num_files, max_amplitude (80), max_audio_length), creates an associated list of transcriptions, and returns the two"""
     #Create a list of filepaths to the audio files
     audio_paths = list(flacfiles(AUDIO_DIR))
     assert num_files <= len(audio_paths) #NOTE: there are 2703 files in LibriSpeech, I think
-    #Grab num_files random audio files and extract their data
+    #Grab num_files random audio files and extract their data as log mel spectrograms
     audio_paths = Locked(audio_paths)
     read_files = Locked([]); file_transcriptions = Locked([])
+    melspectroify = torchaudio.transforms.MelSpectrogram(n_mels=80) #Default values are pretty good for speech (would want to change them for music or another kind of audio)
+    logify = torchaudio.transforms.AmplitudeToDB(top_db=80) #80 is noted as a "reasonable" cutoff by torchaudio API *shrug*
+    logmelspectroify = lambda time_series_audio: logify(melspectroify(time_series_audio))
     with ThreadPoolExecutor(max_workers=NUM_WORKERS) as executor:
-        for _ in range(num_files): executor.submit(extract_data, audio_paths, read_files, file_transcriptions)
+        for _ in range(num_files): executor.submit(extract_data, audio_paths, read_files, file_transcriptions, logmelspectroify)
         executor.shutdown(wait=True)
     read_files = read_files.inner; file_transcriptions = file_transcriptions.inner
     #Add padding to audio data
-    max_len_audio = max(map(lambda seq: len(seq), read_files))
-    padded_audio = list(map(lambda seq: pt.cat((seq, pt.zeros((max_len_audio - len(seq),)))), read_files))
+    max_len_audio = max(map(lambda seq: seq.shape[1], read_files))
+    padded_audio = list(map(lambda seq: pt.cat((seq, pt.zeros((seq.shape[0], max_len_audio - seq.shape[1]))), dim=1), read_files))
     #Tensorify the audio and return
     audio_tensor = pt.stack(padded_audio)
     return (audio_tensor, file_transcriptions)
-    
-    
 
-
+# Quick demo
+if __name__ == "__main__":
+    audio_data, transcriptions  = grab_audio(100)
+    print(f"AUDIO DATA SHAPE: {audio_data.shape}")
+    print("FIRST ENTRY AUDIO:\n", audio_data[0])
+    print("FIRST ENTRY TRANSCRIPTION:\n", transcriptions[0])
