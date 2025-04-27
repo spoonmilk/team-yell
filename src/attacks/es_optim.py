4182a2fa5aeaadcc007de29c5c2f2a322a868edf
spoonmilk <haku31229@gmail.com>
2025-04-27 05:06:33 -0400

    FEAT: WE ARE TRAINING!!!

--- src/attacks/es_optim.py
+++ src/attacks/es_optim.py
@@ -33,7 +33,7 @@ NOISE_MEAN = 0
 NOISE_STD_DEV_RNG_PORTION = 0.05
 MODEL_TYPE = "tiny"
 NUM_WORKERS = 5
-NUM_EPOCHS = 20
+NUM_EPOCHS = 10
 PERFORMANCE_CUTOFF = 0.1
 SCALE_FACTOR = 0.5
 
@@ -41,7 +41,7 @@ SCALE_FACTOR = 0.5
 NUM_LAYERS = 3
 NUM_CHANNELS = 32
 KERNEL_SIZE = 3
-MAX_DELTA = 0.05
+MAX_DELTA = 0.04
 
 try:
     BASE_DIR = Path(__file__).resolve().parent
@@ -82,6 +82,10 @@ def whisper_transcribe(audio_data: pt.Tensor) -> list[str]:
 
 # CHECKED
 def noise_params(model: nn.Module, epoch: int = 0):
+    """
+    Adds noise to the model parameters based on the current epoch.
+    Noise is scaled by the mutation strength, which decreases over time.
+    """
     device = next(model.parameters()).device
     with pt.no_grad():
         strength = mutation_strength(epoch, NUM_EPOCHS)
@@ -91,7 +95,7 @@ def noise_params(model: nn.Module, epoch: int = 0):
 
 # LOGIT LOSS FUNCTIONS
 def extract_logits(perturbed_audio: pt.Tensor):
-    """Takes in a perturbed audio and ground truth transcription, outputting the logits of Whisper's forward pass"""
+    """Takes in a perturbed audio tensor and returns logits from Whisper"""
     tokenizer = whisper.tokenizer.get_tokenizer(
         multilingual=True, task="transcribe", language="en"
     )
@@ -122,14 +126,61 @@ def logit_entropy(logits: pt.Tensor):
     return entropy
 
 
+def compute_logit_reward(
+    clean_audio: pt.Tensor,
+    perturbed_audio: pt.Tensor,
+    logits: pt.Tensor,
+    adversarial_bonus: float = 1.0,
+    distortion_penalty: float = 0.8,
+    hf_incentive: float = 1.0,
+):
+    """
+    Computes a multi-factor reward for the perturbed audio.
+    Logit entropy is our base reward, and we penalize the distortion of the audio, which is mostly in low frequencies.
+    High frequencies are incentivized as they help with intelligibility.
+
+    Args:
+        clean_audio (pt.Tensor): The clean audio tensor.
+        perturbed_audio (pt.Tensor): The perturbed audio tensor.
+        logits (pt.Tensor): The logits from the Whisper model.
+        adversarial_bonus (float): The weight for the logit entropy reward.
+        distortion_penalty (float): The weight for the distortion penalty.
+        hf_incentive (float): The weight for the high frequency incentive.
+    """
+    entropy = logit_entropy(logits)
+    # Take mean squared change between clean and perturbed audio
+    delta = pt.mean(pt.square(perturbed_audio - clean_audio))
+
+    spec = pt.stft(
+        perturbed_audio.squeeze(1), n_fft=256, hop_length=128, return_complex=True
+    )
+    mag = spec.abs()
+
+    # High frequency cutoff
+    # Tune for intelligibility
+    freq_high = 0.95
+    cutoff_idx = int(freq_high * mag.size(1))
+
+    # We incentivize high frequency energy and penalize low frequency energy
+    low_eng = mag[:, : (mag.size(1) // 2)].mean()
+    high_eng = mag[:, cutoff_idx:].mean()
+    hf_ratio = (high_eng + 1e-8) / (low_eng + 1e-8)
+
+    return (
+        adversarial_bonus * entropy
+        - distortion_penalty * delta
+        + hf_incentive * hf_ratio
+    )
+
+
 # CHECKED - NOTE: very heavily penalizes missed words (all words afterwards are considered incorrect)
-def compute_reward(
+def compute_wer_reward(
     clean_transcription: list[str], perturbed_transcription: list[str]
 ) -> list[float]:
     """
     Compute the reward for the perturbed transcription.
 
-    Returns the 'dfitness' of the perturbed transcription.
+    Returns the 'fitness' of the perturbed transcription.
     The fitness is the average WER between the perturbed and clean transcriptions.
     We want WER to be as large as possible
     """
@@ -178,6 +229,16 @@ def update_model_weights(
 def scores_to_weights(
     scores: pt.Tensor, cutoff: float = PERFORMANCE_CUTOFF, scale_factor: float = 0.1
 ) -> pt.Tensor:
+    """
+    Returns the weights for the population based on their scores
+    Scores below the cutoff are set to -inf to filter them out
+    Scale factor helps with softmax stability, tunable
+
+    Args:
+        scores (pt.Tensor): The scores for the population
+        cutoff (float): The cutoff for the scores
+        scale_factor (float): The scale factor for the softmax
+    """
     sorted_scores = pt.sort(scores, descending=True)[0]
     k = max(1, int(cutoff * scores.shape[0]))
     top_scores = sorted_scores[:k].cpu().numpy()
@@ -195,6 +256,18 @@ def scores_to_weights(
 
 
 def mutation_strength(epoch, total_epochs, sig_o=0.5, sig_t=0.01):
+    """
+    Returns the mutation strength for the current epoch
+
+    See simulated annealing. Basic idea is to start with high mutations
+    to get the search moving, then reduce as the model trains to converge.
+
+    Args:
+        epoch (int): The current epoch
+        total_epochs (int): The total number of epochs
+        sig_o (float): The initial mutation strength
+        sig_t (float): The final mutation strength
+    """
     t = epoch / total_epochs
     return sig_o * (1 - t) + sig_t * t
 
@@ -206,6 +279,19 @@ def epoch(
     train_type: str = "transcript",
     epoch: int = 0,
 ):
+    """
+    Run a single epoch of training
+
+    Args:
+        model (WavPerturbationModel): The model to train
+        pop_sz (int): The size of the population
+        batch_sz (int): The size of the batch
+        train_type (str): The type of training to perform ; logit or transcript
+                          transcript = train using WER as reward
+                          logit = train using logit entropy as reward
+                          generally, logit is better
+        epoch (int): The current epoch
+    """
     device = next(model.parameters()).device
     # Grab batch of audio + transcriptions - NOT COMPUTE INTENSIVE
     clean_audio_batch, transcriptions = grab_batch(batch_sz)
@@ -224,11 +310,17 @@ def epoch(
     if train_type == "transcript":
         preds = [whisper_transcribe(x) for x in perturbed_list]
         scores = pt.tensor(
-            [compute_reward(transcriptions, p) for p in preds], device=device
+            [compute_wer_reward(transcriptions, p) for p in preds], device=device
         )
     elif train_type == "logit":
         logits = [extract_logits(x) for x in perturbed_list]
-        scores = pt.tensor([logit_entropy(logit) for logit in logits], device=device)
+        scores = pt.tensor(
+            [
+                compute_logit_reward(clean_audio_batch, perturbed_list[i], logits[i])
+                for i in range(len(perturbed_list))
+            ],
+            device=device,
+        )
         print(
             f"Scores  →  min={scores.min().item():.4f}, "
             f"max={scores.max().item():.4f}, "
@@ -256,6 +348,8 @@ def train_es(
     epochs: int = NUM_EPOCHS,
     type: str = "transcript",
 ):
+    """Train the model using evolutionary strategies"""
+
     print(f"Starting ES training on device={device}")
     for i in range(1, epochs + 1):
         epoch(model, POP_SIZE, BATCH_SIZE, type, epoch=i)
