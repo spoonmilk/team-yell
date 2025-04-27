130c2396f005842e77965313051872f4c066e435
William Stone <william_l_stone@brown.edu>
2025-04-26 20:14:45 -0400

    Changed save and load data to separate training and testing data

--- src/utilities/data_access.py
+++ src/utilities/data_access.py
@@ -5,6 +5,8 @@ import pickle
 from pathlib import Path
 import os
 
+TEST_PORTION = 0.1
+
 try:
     BASE_DIR = Path(__file__).resolve().parent.parent
     DATA_DIR = str(BASE_DIR / "data") + "/"
@@ -14,17 +16,33 @@ except NameError:
 os.makedirs(DATA_DIR, exist_ok=True)
 
 def save_data(waves: torch.Tensor, transcripts: list[str]):
-    torch.save(waves, DATA_DIR + 'waves.pt')
-    with open(DATA_DIR + 'transcripts.pkl', 'wb') as fl:
-        pickle.dump(transcripts, fl)
+    test_num = int(TEST_PORTION*waves.shape[0])
+    indices = random.sample(range(len(transcripts)), test_num)
+    #Separate waves and save them
+    waves = np.squeeze(waves.numpy())
+    test_waves = torch.tensor(np.take(waves, indices, axis=0))
+    train_waves = torch.tensor(np.delete(waves, indices, axis=0))
+    torch.save(test_waves, DATA_DIR + 'test_waves.pt')
+    torch.save(train_waves, DATA_DIR + 'train_waves.pt')
+    #Separate transcripts
+    transcripts = np.array(transcripts)
+    test_trans = list(np.take(transcripts, indices))
+    train_trans = list(np.delete(transcripts, indices))
+    with open(DATA_DIR + 'test_transcripts.pkl', 'wb') as fl:
+        pickle.dump(test_trans, fl)
+    with open(DATA_DIR + 'train_transcripts.pkl', 'wb') as fl:
+        pickle.dump(train_trans, fl)
 
-def load_data() -> tuple[torch.Tensor, list[str]]:
-    waves = torch.load(DATA_DIR + 'waves.pt')
-    with open(DATA_DIR + 'transcripts.pkl', 'rb') as fl:
+def load_data(test: bool = False) -> tuple[torch.Tensor, list[str]]:
+    if test: prefix = 'test'
+    else: prefix = 'train'
+    wave_path = prefix + '_waves.pt'; trans_path = prefix + '_transcripts.pkl'
+    waves = torch.load(DATA_DIR + wave_path)
+    with open(DATA_DIR + trans_path, 'rb') as fl:
         transcripts = pickle.load(fl)
     return waves, transcripts
 
-if Path(DATA_DIR + 'transcripts.pkl').is_file() and Path(DATA_DIR + 'waves.pt').is_file():
+if Path(DATA_DIR + 'test_transcripts.pkl').is_file() and Path(DATA_DIR + 'train_waves.pt').is_file():
     waves, transcripts = load_data()
     assert len(waves) == len(transcripts)
 
