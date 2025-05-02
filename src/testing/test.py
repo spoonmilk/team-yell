8c27dc9cddd0945225b79daa50b682aba11883ac
spoonmilk <haku31229@gmail.com>
2025-05-02 00:27:26 -0400

    fix: whisper device loading fix

--- src/testing/test.py
+++ src/testing/test.py
@@ -32,30 +32,31 @@ except NameError:
     CHECKPOINTS_DIR = os.path.abspath("../attacks/checkpoints") + "/"
     TEMP_DIR = os.path.abspath("../temp") + "/"
 
-#NOTE: API keys must be added into a .env file at in this testing folder in order to run any of the Assembly AI, Gladia, or Speechmatics tests
-if not load_dotenv(): print("WARNING: You are running this script without a .env file with API keys, so you will only be able to run Whisper related test functions.")
+# NOTE: API keys must be added into a .env file at in this testing folder in order to run any of the Assembly AI, Gladia, or Speechmatics tests
+if not load_dotenv():
+    print(
+        "WARNING: You are running this script without a .env file with API keys, so you will only be able to run Whisper related test functions."
+    )
 AAI_API_KEY = os.environ.get("AAI_API_KEY")
 GLADIA_API_KEY = os.environ.get("GLADIA_API_KEY")
 SPEECHMATICS_API_KEY = os.environ.get("SPEECHMATICS_API_KEY")
 
-#Load in testing data
+# Load in testing data
 test_waves, test_transcripts = load_data(test=True)
 test_waves = test_waves[:50]
 test_transcripts = test_transcripts[:50]
-#Create noise filtered testing data
-transform = AddGaussianNoise(
-    min_amplitude=0.03,
-    max_amplitude=0.045,
-    p=1.0
-)
+# Create noise filtered testing data
+transform = AddGaussianNoise(min_amplitude=0.03, max_amplitude=0.045, p=1.0)
 noisy_waves = transform(test_waves, 16000)
 
 # GENERAL FUNCTIONS
+
 
 def grab_perturbation_model(rel_path: str) -> pt.nn.Module:
     """Given the name of a model file in the checkpoints directory, loads that model in and returns it"""
     perturbation_model = pt.load(CHECKPOINTS_DIR + rel_path)
     return perturbation_model
+
 
 def test_audio_set(
     audio: pt.Tensor,
@@ -82,9 +83,10 @@ def test_audio_set(
     mean_wer = np.mean(wers)
     return mean_wer
 
+
 # Typing left somewhat ambiguous because task return type is so variable
 def try_until(task: Callable[[Any], Any], args: tuple, err_msg: str) -> Any:
-    """Given a function (task) and a tuple of arguments for it, calls the function continuously until it executes 
+    """Given a function (task) and a tuple of arguments for it, calls the function continuously until it executes
     without error, printing an error message on each failure."""
     while True:
         try:
@@ -96,12 +98,15 @@ def try_until(task: Callable[[Any], Any], args: tuple, err_msg: str) -> Any:
             break
     return task_result
 
+
 # ASSEMBLY AI TEST FUNCTIONS
+
 
 def curried_test_one_aai(
     config: aai.TranscriptionConfig,
 ) -> Callable[[pt.Tensor, str], float]:
     """Returns a version of test_one_aai that uses the specified TranscriptionConfig object"""
+
     def test_one_aai(wave: pt.Tensor, trans: str) -> float:
         """Returns the WER  (with respect to trans) of an Assembly AI model's output when wave is fed through it"""
         dimmed_wave = wave.unsqueeze(0)
@@ -114,19 +119,26 @@ def curried_test_one_aai(
             return 0
         test_wer = wer(trans, aai_transcript.text)
         return test_wer
+
     return test_one_aai
+
 
 def test_aai(perturbation_model: pt.nn.Module, aai_level: str = "best"):
     """Tests Assembly AI's performance on clean audio compared to its performance on audio perturbed by the input
-    perturbation model, printing out the results found. The specific Assembly AI model is specified by aai_level, 
+    perturbation model, printing out the results found. The specific Assembly AI model is specified by aai_level,
     which may be set to 'nano' or 'best'."""
     # Set API key
-    assert AAI_API_KEY, "No Assembly AI API key in .env file, so cannot run Assembly AI tests"
+    assert AAI_API_KEY, (
+        "No Assembly AI API key in .env file, so cannot run Assembly AI tests"
+    )
     aai.settings.api_key = AAI_API_KEY
     # Grab appropriate model config
-    if aai_level == "best": config = aai.TranscriptionConfig(speech_model=aai.SpeechModel.best)
-    elif aai_level == "nano": config = aai.TranscriptionConfig(speech_model=aai.SpeechModel.nano)
-    else: raise Exception(f"Invalid aai model ({aai_level}) specified")
+    if aai_level == "best":
+        config = aai.TranscriptionConfig(speech_model=aai.SpeechModel.best)
+    elif aai_level == "nano":
+        config = aai.TranscriptionConfig(speech_model=aai.SpeechModel.nano)
+    else:
+        raise Exception(f"Invalid aai model ({aai_level}) specified")
     # Get curried test function
     test_one_aai = curried_test_one_aai(config)
     # Test model on each unperturbed audio clip
@@ -141,7 +153,9 @@ def test_aai(perturbation_model: pt.nn.Module, aai_level: str = "best"):
         f"AAI {aai_level} MODEL WER DEPROVEMENT WITH PERTURBATION: {mean_wer_perturbed - mean_wer_unperturbed}"
     )
 
+
 # WHISPER TEST FUNCTIONS
+
 
 def whisper_transcribe(w_model: whisper.Whisper, audio: pt.Tensor) -> list[str]:
     """Given a Whisper model and a tensor of audio clips, returns a list of corresponding Whisper transcriptions"""
@@ -151,25 +165,35 @@ def whisper_transcribe(w_model: whisper.Whisper, audio: pt.Tensor) -> list[str]:
     results = whisper.decode(w_model, mel, opts)
     return [result.text for result in results]
 
+
 def test_set_whisper(
     w_model: whisper.Whisper, audio: pt.Tensor, transcripts: list[str]
 ) -> float:
     """Returns the mean WER of the given Whisper model across the all audio clips in the input tensor, referrencing
     the input list of corresponding transcripts as a ground truth"""
     whisper_transcripts = whisper_transcribe(w_model, audio)
-    wers = np.array(list(map(lambda w_trans, trans: wer(trans, w_trans), whisper_transcripts, transcripts)))
+    wers = np.array(
+        list(
+            map(
+                lambda w_trans, trans: wer(trans, w_trans),
+                whisper_transcripts,
+                transcripts,
+            )
+        )
+    )
     return np.mean(wers)
+
 
 def test_whisper(
     perturbation_model: pt.nn.Module, whisper_level: str = "tiny", noisy: bool = False
 ):
     """Tests Whisper's performance on clean audio compared to its performance on audio perturbed by the input
-    perturbation model, printing out the results found. The specific Whisper model is specified by aai_level, 
-    which may be set to 'nano' or 'best'. Alternatively, noisy may be set to true to test Whisper against a 
+    perturbation model, printing out the results found. The specific Whisper model is specified by aai_level,
+    which may be set to 'nano' or 'best'. Alternatively, noisy may be set to true to test Whisper against a
     plain noise filter"""
     print("Loading model at level:", whisper_level)
     whisper_model = whisper.load_model(whisper_level)
-    whisper_model.device = "cpu"
+    # whisper_model.device = "cpu"
     print("Model loaded, running test_set_whisper with the test data")
     unperturbed_wer = test_set_whisper(whisper_model, test_waves, test_transcripts)
     print(f"WHISPER {whisper_level} MODEL UNPERTURBED MEAN_WER: {unperturbed_wer}")
@@ -229,10 +253,12 @@ def test_whisper(
             f"STOI scores range: min={min(stoi_scores):.4f}, max={max(stoi_scores):.4f}"
         )
 
+
 # GLADIA TESTING FUNCTIONS
 
+
 def request_transcript(headers: dict, data: dict) -> str:
-    """Given a generic dictionary of post headers and a data dict containing the URL to a previously uploaded audio 
+    """Given a generic dictionary of post headers and a data dict containing the URL to a previously uploaded audio
     clip retrieves Gladia's transcription of that piece of audio"""
     trans_req_resp = requests.post(
         "https://api.gladia.io/v2/pre-recorded/", headers=headers, json=data
@@ -296,6 +322,7 @@ def test_one_gladia(wave: pt.Tensor, trans: str) -> float:
     test_wer = wer(trans, g_trans)
     return test_wer
 
+
 def test_gladia(perturbation_model: pt.nn.Module):
     """Tests Gladia's performance on clean audio compared to its performance on audio perturbed by the input
     perturbation model, printing out the results found."""
@@ -310,22 +337,27 @@ def test_gladia(perturbation_model: pt.nn.Module):
         f"GLADIA MODEL WER DEPROVEMENT WITH PERTURBATION: {perturbed_wer - unperturbed_wer}"
     )
 
+
 # SPEECHMATICS TEST FUNCTIONS
 
+
 def speechmatics_transcribe(client: BatchClient, file_path: str, conf: dict) -> str:
-    """Given a Speechmatics client, a configuration for it, and a file_path to a saved audio file, returns the 
+    """Given a Speechmatics client, a configuration for it, and a file_path to a saved audio file, returns the
     Speechmatics' model's transcription of the file"""
     try:  # CREDIT: Only slightly modified from the speechmatics API documentation
         job_id = client.submit_job(audio=file_path, transcription_config=conf)
         s_transcript = client.wait_for_completion(job_id, transcription_format="txt")
     except HTTPStatusError as e:
         if e.response.status_code == 401:
-            print(f"Invalid API key. Make sure {SPEECHMATICS_API_KEY} is in fact a working API key")
+            print(
+                f"Invalid API key. Make sure {SPEECHMATICS_API_KEY} is in fact a working API key"
+            )
         elif e.response.status_code == 400:
             print(e.response.json()["detail"])
         else:
             raise e
     return s_transcript
+
 
 def curried_test_one_speechmatics(settings: ConnectionSettings, conf: dict) -> float:
     """Returns a version of test_one_speechmatics set up to use a specified ConnectionSetting and configuration
@@ -356,10 +388,13 @@ def curried_test_one_speechmatics(settings: ConnectionSettings, conf: dict) -> f
 
     return test_one_speechmatics
 
+
 def test_speechmatics(perturbation_model: pt.nn.Module):
     """Tests Speechmatics' performance on clean audio compared to its performance on audio perturbed by the input
     perturbation model, printing out the results found."""
-    assert SPEECHMATICS_API_KEY, "No Speechmatics API key in .env file, so cannot run Gladia tests"
+    assert SPEECHMATICS_API_KEY, (
+        "No Speechmatics API key in .env file, so cannot run Gladia tests"
+    )
     settings = ConnectionSettings(
         url="https://asr.api.speechmatics.com/v2",
         auth_token=SPEECHMATICS_API_KEY,
@@ -380,12 +415,17 @@ def test_speechmatics(perturbation_model: pt.nn.Module):
         f"SPEECHMATICS MODEL WER DEPROVEMENT WITH PERTURBATION: {perturbed_wer - unperturbed_wer}"
     )
 
+
 # MAIN BEHAVIOR
 
 if __name__ == "__main__":
-    model = grab_perturbation_model("wavperturbation_model.pt") #Alter model file name as needed
+    model = grab_perturbation_model(
+        "wavperturbation_model.pt"
+    )  # Alter model file name as needed
     # Uncomment the tests you want to run:
     # test_aai(model, aai_level="best") #OPTIONS: "nano", "best"
     # test_speechmatics(model)
     # test_gladia(model)
-    # test_whisper(model, whisper_level="tiny") #OPTIONS: "base", "tiny", "medium", "large", "turbo"
+    test_whisper(
+        model, whisper_level="tiny"
+    )  # OPTIONS: "base", "tiny", "medium", "large", "turbo"
